14fd0f41e0cc5b9adbe6a092ca37f362041d9feb
Rohan Dhanam <Rohan>
2024-01-09 23:14:15 -0500

    first javascript function, added github repository link

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+function toggleRepoLink() {
+    var repoLink = document.getElementById("repoLink");
+    repoLink.style.display = (repoLink.style.display === "none" || repoLink.style.display === "") ? "block" : "none";
+}
